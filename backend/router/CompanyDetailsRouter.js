a414f4811a61e39593bc97cd0cc16660b131743f
laxit_khanpara <laxitkhanpara3646@gmail.com>
2024-06-19 13:54:10 +0530

    new

--- backend/router/CompanyDetailsRouter.js
+++ backend/router/CompanyDetailsRouter.js
@@ -6,10 +6,10 @@ const axios = require('axios');
 console.log(1234);
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE4NzgzODcyLCJpYXQiOjE3MTg3ODM1NzIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjZlYjE5NzVkLTAwYjItNDUxOS05YzU3LWJhZGMwNGJkMDUzZiIsInN1YiI6IjIxMDMwMzEwODAzOUBwYXJ1bHVuaXZlcnNpdHkuYWMuaW4ifSwiY29tcGFueU5hbWUiOiJnb01hcnQiLCJjbGllbnRJRCI6IjZlYjE5NzVkLTAwYjItNDUxOS05YzU3LWJhZGMwNGJkMDUzZiIsImNsaWVudFNlY3JldCI6InZLSFpYQU5JWWhOSXVzWU0iLCJvd25lck5hbWUiOiJMYXhpdCBLaGFucGFyYSIsIm93bmVyRW1haWwiOiIyMTAzMDMxMDgwMzlAcGFydWx1bml2ZXJzaXR5LmFjLmluIiwicm9sbE5vIjoiMjEwMzAzMTA4MDM5In0.di4fwYA508JinVHlK6iZAACxPhZehvRAO-MZRwUiD9A'
 // Route to fetch data from the API
-let products
+let products = []
 router.get(`/categories/:categoryname/products`, async (req, res) => {
     try {
-        const categoryname=req.params.categoryname
+        const categoryname = req.params.categoryname
         console.log(categoryname);
         // console.log(123);
         // const {} =req.body
@@ -42,28 +42,12 @@ router.get(`/categories/:categoryname/products`, async (req, res) => {
 
 router.get(`/categories/:categoryname/products/:productid`, async (req, res) => {
     try {
-        const categoryname=req.params.categoryname
-        const productid=req.params.productid
-
-        console.log(categoryname);
-        console.log(`Bearer ${token}`);
-        // const apiEndpoint = 'http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&minPrice=1&maxPrice=10000';
-        const apiEndpoint = `http://20.244.56.144/test/companies/AMZ/categories/${categoryname}/products?top=10&minPrice=1&maxPrice=10000`;
-
-
-        const response = await axios.get(apiEndpoint, {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-
-     
-        console.log('Data from API:', response);
-
-        response.data.sort((a, b) => b.rating - a.rating);
-        
-
-        res.json(products);
+        const categoryname = req.params.categoryname
+        const productid = req.params.productid
+        console.log(products);
+        const product= products.find(productx => productx.id === productid && productx.category === categoryname);        
+        console.log(product);
+        res.json(product);
     } catch (error) {
         console.error('Error fetching data:', error.message);
         res.status(500).json({ error: 'Failed to fetch data' });
